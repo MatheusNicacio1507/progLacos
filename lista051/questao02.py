62b3e1d78e7c38db001bbe71b760fcd966466d1e
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-21 09:14:48 -0300

    questão 2

---
+++ lista051/questao02.py
@@ -0,0 +1,9 @@
+'''
+Desenvolver um programa que exiba os números em ordem decrescente de 100 até 1.
+'''
+
+contador = 100
+
+while (contador >= 1):
+    print(f"Contador: {contador}")
+    contador = contador -1
