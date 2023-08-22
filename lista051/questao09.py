7b27d539a6163e55560c2e9a2c1f0e37ef755500
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-22 08:13:38 -0300

    questão 9

--- lista051/questao09.py
+++ lista051/questao09.py
@@ -2,3 +2,16 @@
 Elaborar um programa que apresente no final a soma dos valores pares existentes na faixa de 0 até 500. Utilize um laço que efetue a variação de 2 em 2.
 '''
 
+contador = 2
+
+acumulador = 0
+
+while(contador <= 500):
+    par = contador%2
+
+    if(par == 0):
+        acumulador = acumulador + contador
+        contador = contador + 2
+
+print(f" A soma dos números pares é {acumulador}")
+
