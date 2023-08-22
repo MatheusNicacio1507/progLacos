fa4c7db987375268e1a9f906412f8fd9e813cb18
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-22 11:41:54 -0300

    questão 12

--- lista051/questao12.py
+++ lista051/questao12.py
@@ -2,18 +2,30 @@
 Desenvolver um programa que peça ao usuário para digitar diversos números reais, e ao final, exibir o maior e o menor número que foram digitados, além da média entre TODOS os números digitados pelo usuário. A inserção de números deve parar quando o usuário digitar o número -1, e este número -1 não deve ser considerado nem como maior, nem como menor, e nem na contagem da média.
 '''
 
-num = int(input("Insira um número: "))
+num = float(input("Insira um número(-1 Encerra o programa): "))
 
-while (contador != -1):
-    num = int(input("Insira outro número: "))
+maior = num
+menor = num
+total_respostas = 0 #contador
+soma = 0 #acumulador
 
-    maior = contador
+while (num != -1):
+    soma = soma + num
+    total_respostas = total_respostas + 1
 
-    if (maior < num2):
-        maior = num2
+    if (maior < num):
+        maior = num
 
-    if (maior < num3):
-        maior = num3
+    if(menor > num):
+        menor = num
 
-    print(f"O maior valor inserido foi {maior}")
-    print(f"O menor valor inserido foi {menor}")
+    num = float(input("Insira outro número(-1 Encerra o programa): "))
+
+if(maior == -1):
+    print("Você inseriu -1 na primeira resposta.\nPROGRAMA ENCERRADO")
+
+else:
+    print(f"Maior valor inserido: {maior}")
+    print(f"Maior valor inserido: {menor}")
+    print(f"Média dos valores inseridos = {soma / total_respostas}")
+    print("FIM DO PROGRAMA")
---
+++ lista051/questao13.py
@@ -0,0 +1,13 @@
+'''
+Desenvolver um programa que imprima a tabuada de 3 a 6.
+'''
+
+contador = 3
+
+acumulador = 1
+
+while(contador <= 6):
+    tab = contador * acumulador
+    acumulador = acumulador + 1
+    while():
+
